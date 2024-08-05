38df0b21ca0ff7bd84cf2a3bc1e89507178d800a
Hamza Islam <starhax4@gmail.com>
2024-08-05 18:57:13 +0500

    added revenue chart and latest invoice card to dashboard -e

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -24,6 +24,7 @@ export async function fetchRevenue() {
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
+    
     throw new Error('Failed to fetch revenue data.');
   }
 }
