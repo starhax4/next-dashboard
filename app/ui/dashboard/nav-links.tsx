3f9100309c7148358776e13bbbee2d1570446727
Hamza Islam <starhax4@gmail.com>
2024-08-06 14:43:36 +0500

    enabled nav-link activated state

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -7,7 +7,7 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-// import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
@@ -23,7 +23,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  // const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -34,9 +34,9 @@ export default function NavLinks() {
             href={link.href}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              // {
-              //   'bg-sky-100 text-blue-600': pathname === link.href,
-              // },
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
             )}
           >
             <LinkIcon className="w-6" />
