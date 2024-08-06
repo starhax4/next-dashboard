b9f71e2784c6239d93bea641e3a8cb6b5dcbf85e
Hamza Islam <starhax4@gmail.com>
2024-08-06 19:21:57 +0500

    added debuging log to action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,12 @@
-'use server';
- 
+"use server";
+
 export async function createInvoice(formData: FormData) {
+  console.log("Action is started!");
+
   const rawFormData = {
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   };
   // Test it out:
   console.log(rawFormData);
