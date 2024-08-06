10f205ff3a2d97f4468559ae3fabc2375f7a5caf
Hamza Islam <starhax4@gmail.com>
2024-08-06 15:26:06 +0500

    added search pathname and params to update url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,11 +1,22 @@
-'use client';
+"use client";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function Search({ placeholder }: { placeholder: string }) {
-
+  const searchParams = useSearchParams();
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
